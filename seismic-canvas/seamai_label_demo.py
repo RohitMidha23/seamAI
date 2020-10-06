0515430320df17663bd87820015fd93ca0e145a8
shraddhaamohan <shraddhaa.mohan@gmail.com>
2020-10-06 17:23:22 +0530

    revert

--- seismic-canvas/seamai_label_demo.py
+++ seismic-canvas/seamai_label_demo.py
@@ -10,8 +10,7 @@ import argparse
 
 import numpy as np
 from vispy.color import get_colormap, Colormap, Color
-from vispy.gloo.util import _screenshot
-from vispy import scene, io
+from vispy import scene
 
 from seismic_canvas import (SeismicCanvas, volume_slices, XYZAxis, Colorbar)
 
@@ -23,9 +22,7 @@ my_parser.add_argument('--filepath',
                       # metavar='path',
                        type=str,
                        help='the path to file')
-my_parser.add_argument('--nbmode',
-                        default=0,
-                       help='whether notebook mode')
+
 
 if __name__ == '__main__':
 
@@ -40,7 +37,8 @@ if __name__ == '__main__':
   axis_scales = (0.7, 0.5, 1) # anisotropic axes (stretch z-axis)
 
   # Colormaps.
-  cmap=Colormap([[1,0,0], [0,1,0], [0,0,1]]); clim=(1, 6)
+  cmap=Colormap([[1,0,0], [0,1,0], [0,0,1]])
+  clim=(1, 6)
   # Get visual nodes ready.
   visual_nodes = volume_slices(volume,
     cmaps=cmap, clims=clim,
@@ -68,9 +66,6 @@ if __name__ == '__main__':
                          azimuth=45,
                          zoom_factor=1.2 # >1: zoom in; <1: zoom out
                          )
-  if args.nbmode == 0:
-      canvas.measure_fps()
-      canvas.app.run()
-  else:
-    screenshot = _screenshot()
-    io.write_png("labels_example"+ '.png', screenshot)
+  canvas.measure_fps()
+  canvas.app.run()
+
